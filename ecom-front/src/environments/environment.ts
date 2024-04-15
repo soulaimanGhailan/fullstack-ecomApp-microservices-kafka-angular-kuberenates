8408d8775ce710db728ddf687617e571a60a96a6
soulaiman ghailan <soulaiman8ghailan@gmail.com>
2024-04-15 12:14:48 +0100

    helm chart helmfile

--- ecom-front/src/environments/environment.ts
+++ ecom-front/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  productService:"http://10.10.9.133:31506" ,
-  customerService:"http://10.10.9.133:31946" ,
+  productService:"http://10.10.9.133:30010" ,
+  customerService:"http://10.10.9.133:30020" ,
   Auth_Test_Customer : {
     customerId: "test-customer" , firstname: "soulaiman" ,lastname:"ghailan" , email:"soulaiman" , phoneNumber:"03666363666"
   }
